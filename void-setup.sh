4162956d918edf0207b538b980fc4342979d6419
enriicola <enriicola@proton.me>
2025-03-18 14:38:37 +0100

    refactor

--- void-setup.sh
+++ void-setup.sh
@@ -2,7 +2,17 @@
 
 # sudo xbps-install -Sy curl
 
-void_packages=("git" "wget" "htop" "neofetch" "fastfetch" "btop" "vpm" "fzf" "github-cli")
+void_packages=(
+   "git"
+   "wget"
+   "htop"
+   "neofetch"
+   "fastfetch"
+   "btop"
+   "vpm"
+   "fzf"
+   "github-cli"
+)
 
 for package in "${void_packages[@]}"; do
    if ! sudo xbps-install -Sy "$package"; then
