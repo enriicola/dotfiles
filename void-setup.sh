4e043c4fb8908682d86aac4572b82d252b7c1c97
enriicola <enriicola@proton.me>
2025-03-18 14:36:53 +0100

    add error handling

--- void-setup.sh
+++ void-setup.sh
@@ -5,7 +5,10 @@
 void_packages=("git" "wget" "htop" "neofetch" "fastfetch" "btop" "vpm" "fzf" "github-cli")
 
 for package in "${void_packages[@]}"; do
-   sudo xbps-install -S "$package"
+   if ! sudo xbps-install -Sy "$package"; then
+      echo "Failed to install $package"
+      exit 1
+fi
 done
 
 git config --global init.defaultBranch main
