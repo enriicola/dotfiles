28db98753403341b9c0a3afa44a7111c137481c5
enriicola <enriicola@proton.me>
2025-03-18 14:33:39 +0100

    it was the dash ...

--- void-setup.sh
+++ void-setup.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-sudo xbps-install -Sy curl
+# sudo xbps-install -Sy curl
 
-void-packages=("git" "wget" "htop" "neofetch" "fastfetch" "btop" "vpm" "fzf" "github-cli")
-    
-for package in "${!void-packages[@]}"; do
-   sudo xbps-install -S $package
+void_packages=("git" "wget" "htop" "neofetch" "fastfetch" "btop" "vpm" "fzf" "github-cli")
+
+for package in "${void_packages[@]}"; do
+   sudo xbps-install -S "$package"
 done
-
-for package in ${!void-packages[@]}; do
-   sudo xbps-install -Sy ${void-packages[$package]}
-done
-
 
 git config --global init.defaultBranch main
