0f736e08e7445b17c78565468032c2ad86003473
enriicola <enriicola@proton.me>
2025-03-19 22:41:46 +0100

    fix autologin test

--- test-autologin.sh
+++ test-autologin.sh
@@ -1,1 +1,1 @@
-sudo sed -i 's/^GETTY_ARG="--noclear"/GETTY_ARG=--noclear -n -o enriicola"/' /etc/sv/agetty-tty1/conf"
+sudo sed -i 's/^GETTY_ARGS="--noclear"/GETTY_ARGS="--noclear -n -o enriicola"/' /etc/sv/agetty-tty1/conf
