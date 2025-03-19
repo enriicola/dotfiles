08c7b8e10f993911243c5b22e58e5fb1d7141938
enriicola <enriicola@proton.me>
2025-03-19 23:02:02 +0100

    fix autologin test

--- test-autologin.sh
+++ test-autologin.sh
@@ -1,3 +1,1 @@
-# use sed to change GETTY autologin
-
-sudo sed -i 's/^#.*GETTY_ARGS.*$/GETTY_ARGS="--no-clear -n -o enriicola"/' /etc/sv/agetty-tty1/conf
+sudo sed -i 's/^GETTY_ARGS=.*/GETTY_ARGS="--no-clear -n -o enriicola"/' /etc/sv/agetty-tty1/conf
